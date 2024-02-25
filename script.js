fc7b2ddbecc18e790c3a2cd2201df9091edd316e
Aoitrad <dzaky.ahmad8414@gmail.com>
2024-02-25 20:49:33 +0700

    modif sedikit js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (usia < 0 || usia > 100 || isNaN(usia)) {
         alert("Usia Anda belum cukup.");
     } else {
         // Pilih studio A, B, atau C
-        var pilihanStudio = prompt("Pilih studio: A, B, atau C");
+        var pilihanStudio = prompt("Pilih studio: A, B, atau C (Gunakan Huruf Kapital)");
         if (pilihanStudio === "A" || pilihanStudio === "B" || pilihanStudio === "C") {
             alert(`Selamat datang \n Tiket atas nama ${nama} dengan usia ${usia} tahun \n Anda telah memilih studio ${pilihanStudio} `);
         } else {
